f5bd7af506ba39ce5336aa02e32a2ede5c662ea6
irinamironova <mayinyard@gmail.com>
2020-12-09 23:13:53 +0300

    Второе задание по типам данных

---
+++ src/Exercise2.java
@@ -0,0 +1,20 @@
+public class Exercise2 {
+
+    public void createVariable(){
+        double d = Double.MAX_VALUE;
+        System.out.println(d);
+
+        d = 12.46;
+        System.out.println(d);
+
+
+        int b = Integer.MIN_VALUE;
+        System.out.println(b);
+
+        b = Integer.MAX_VALUE;
+        System.out.println(b);
+
+    }
+
+
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Exercise2 exercise = new Exercise2();
+            exercise.createVariable();
     }
 }
